eb307568ea19eed76608cbfa3b4121cd253a58f9
yahia-nasser <yahia.nasser23@gmail.com>
2025-11-12 21:58:32 +0400

    api fixing

--- src/apis/getAllProducts.ts
+++ src/apis/getAllProducts.ts
@@ -1,5 +1,5 @@
 export default async function getAllProducts() {
-  const response = await fetch("http://localhost:3000/api/users", {
+  const response = await fetch("https://ecommerce.routemisr.com/api/users", {
     cache: "force-cache",
   });
   const data = await response.json();
